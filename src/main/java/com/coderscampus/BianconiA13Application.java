9e394d71b2b4e58b9e5086069e6075a634e74a24
HA-Kodes <2BianconiHa@gmail.com>
2025-11-11 18:57:10 -0800

    Initial commit

---
+++ src/main/java/com/coderscampus/BianconiA13Application.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BianconiA13Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BianconiA13Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/coderscampus/BianconiA13ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.coderscampus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BianconiA13ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

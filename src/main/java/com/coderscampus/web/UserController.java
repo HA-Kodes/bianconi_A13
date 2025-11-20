4014f1a01d9b060231466e18327bf22e6863995d
HA-Kodes <2BianconiHa@gmail.com>
2025-11-19 18:30:56 -0800

    Modify /users Endpoint

--- src/main/java/com/coderscampus/web/UserController.java
+++ src/main/java/com/coderscampus/web/UserController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -55,7 +56,7 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/{userId}")
-	public String postOneUser (User user) {
+	public String postOneUser (@ModelAttribute User user) {
 		userService.saveUser(user);
 		return "redirect:/users/"+user.getUserId();
 	}
